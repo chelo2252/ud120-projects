5a41c5701ed75b4c32c1ab3486fc5daea2cb9042
Marcelo Cardozo <chelo2252the@gmail.com>
2017-05-26 15:48:52 -0400

    SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,33 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100]
 
+from sklearn.svm import SVC
 
+classifier = SVC(kernel='rbf', C=10000.)
 
-#########################################################
-### your code goes here ###
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "\ntraining time:", round(time()-t0, 3), "s\n"
 
-#########################################################
+t0 = time()
+labels_predicted = classifier.predict(features_test)
+print "\nprediction time:", round(time()-t0, 3), "s\n"
+
+accuracy = classifier.score(features_test, labels_test)
+
+print 'Accuracy: ', accuracy 
+
+chris = 0
+sara = 0
+for prediction in labels_predicted:
+	if(prediction == 1):
+		chris +=1
+	else:
+		sara +=1
+
+print 'Chris: ', chris, '\n', 'Sara: ', sara, '\n'
 
 
